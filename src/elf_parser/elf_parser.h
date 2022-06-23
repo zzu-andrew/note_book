7417c65a3155cb71b8fb5dbf94ebbd66c1504c7f
andrewgithub <Wang.159>
2022-06-23 19:42:11 +0800

    添加单例实现实现说明

---
+++ src/elf_parser/elf_parser.h
@@ -0,0 +1,50 @@
+//
+// Created by wangyz38535 on 2022/6/23.
+//
+
+#ifndef NODE_BOOKS_ELF_PARSER_H
+#define NODE_BOOKS_ELF_PARSER_H
+
+
+
+#include <cstdint>
+#include <string>
+
+namespace ELF
+{
+
+    class ELF_reader
+    {
+    public:
+        ELF_reader();
+        explicit ELF_reader(const std::string& file_path);
+        ELF_reader(const ELF_reader& object) = delete;
+        ELF_reader(ELF_reader&& object) noexcept;
+        ELF_reader& operator=(const ELF_reader& object) = delete;
+        ELF_reader& operator=(ELF_reader&& object) noexcept;
+        ~ELF_reader();
+
+        void load_file(const std::string& file_path);
+
+        void show_file_header() const;
+        void show_section_headers() const;
+        void show_symbols() const;
+
+    private:
+        void load_memory_map();
+        void close_memory_map();
+        void initialize_members(std::string file_path = std::string(),
+                                int fd = -1,
+                                std::size_t program_length = 0,
+                                std::uint8_t *mmap_program = nullptr);
+
+        std::string file_path_;
+        int fd_;
+        std::size_t program_length_;
+        std::uint8_t *mmap_program_;
+    };
+
+} // namespace ELF
+
+
+#endif //NODE_BOOKS_ELF_PARSER_H
