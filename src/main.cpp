74087655bc3fe3ef1f396ecb8df044171b614c91
andrewgithub <Wang.159>
2023-05-22 14:52:30 +0800

    添加数据库部分

--- src/main.cpp
+++ src/main.cpp
@@ -49,25 +49,22 @@ using namespace std;
 #include <cstring>
 
 
-
-
-
-
-
-class Data {
-public:
-    void Show() const ;
-
-
-private:
-    int name;
-
+struct X {
+    typedef float type;
 };
 
+struct Y {
+    typedef float type2;
+};
 
+template <typename T, typename U>
+void foo(T t, typename U::type u) {
+    // ...
+}
 
-void Data::Show() const {
-    std::cout << "name : " << name << std::endl;
+void callFoo() {
+    foo<int, X>(5, 5.0); // T == int, typename U::type == X::type == float
+    foo<int, Y>(5, 5.0); // ???
 }
 
 int main(int argc, char **argv)
