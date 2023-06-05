3675916696c311c8a82fd6a05b986efcea15426e
andrewgithub <Wang.159>
2023-06-05 19:03:29 +0800

    添加结构体被初始化列表初始成员变量之后，默认生成的构造函数种类，以及对应的问题

--- src/main.cpp
+++ src/main.cpp
@@ -8,74 +8,31 @@
 #include <unordered_map>
 #include <exception>
 #include <mutex>
+#include <cstddef>
 
 //#include <arpa/inet.h>
 
 using namespace std;
 
-
-
-int isLittleEndian() {
-    static unsigned short data = 0x1234;
-    if (*((char*)&data) == 0x34) {
-        return 1;
+class f{
+public:
+    void Show() {
+        std::cout << "ffffffffffffffffffffffffffffffffffffff" << std::endl;
     }
-    else {
-        return 0;
-    }
-}
-
-
-struct Data {
-    union {
-        uint32_t res;
-        struct {
-           uint8_t res1 : 4;
-           uint8_t res2 : 4;
-           uint8_t res3 : 4;
-           uint8_t res4 : 4;
-           uint8_t res5 : 4;
-           uint8_t res6 : 4;
-           uint8_t res7 : 4;
-           uint8_t res8 : 4;
-        };
-    };
 };
 
-std::mutex muix;
-
-void LockData(std::unique_lock<std::mutex>* lpLock)
-{
-    std::unique_lock<std::mutex> lodck(muix, std::defer_lock);
-
-    std::cout <<  lpLock->owns_lock() << std::endl;
-
-}
-
-
-bool SafeEqual(const std::string& a, const std::string& b) {
-    if (a.length() != b.length()) {
-        return false;
+class son : public f {
+public:
+    void Show(){
+        std::cout << "+++++++++++++++++++++++++++++++++++++++" << std::endl;
     }
-
-
-    auto length = a.length();
-    int equal  = 0;
-    for (auto i = 0; i < length; i++) {
-        equal |= a[i] ^ b[i];
-    }
-
-    return equal == 0;
-}
+};
 
 int main(int argc, char **argv)
 {
-
-    std::unique_lock<std::mutex> lodk(muix);
-    LockData(&lodk);
-
-    SafeEqual("data","data");
-
-
+    son s;
+    f *lp = &s;
+    lp->Show();
+    ((son*) lp)->Show();
     return 0;
 }
