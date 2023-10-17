fd08da121b9c739e50771f2b87fd655eb3b32ae9
zzu-andrew <564631192@qq.com>
2023-10-17 17:21:26 +0800

    更新笔记

--- src/main.cpp
+++ src/main.cpp
@@ -1,124 +1,22 @@
 #include <iostream>
-#include <vector>
-#include <queue>
-#include <thread>
-#include <mutex>
-#include <condition_variable>
-#include <functional>
-#include <atomic>
-#include <chrono>
-#include <stdexcept>
-
-class ThreadPool {
-public:
-    ThreadPool(size_t numThreads = std::thread::hardware_concurrency()) :
-            stop(false), num_idle_threads(0), num_threads_executing(0) {
-        if (numThreads == 0) {
-            throw std::invalid_argument("Invalid number of threads.");
-        }
-        for (size_t i = 0; i < numThreads; ++i) {
-            workers.emplace_back(
-                    [this] {
-                        for (;;) {
-                            std::function<void()> task;
-                            {
-                                std::unique_lock<std::mutex> lock(this->queue_mutex);
-                                this->condition.wait(lock,
-                                                     [this] { return this->stop || !this->tasks.empty(); });
-                                if (this->stop && this->tasks.empty())
-                                    return;
-                                if (this->tasks.empty()) {
-                                    ++this->num_idle_threads;
-                                    this->condition.wait(lock, [this] { return this->stop || !this->tasks.empty(); });
-                                    --this->num_idle_threads;
-                                }
-                                if (!this->tasks.empty()) {
-                                    task = std::move(this->tasks.front());
-                                    this->tasks.pop();
-                                    ++this->num_threads_executing;
-                                }
-                            }
-                            if (task) {
-                                task();
-                                --this->num_threads_executing;
-                            }
-                        }
-                    }
-            );
-        }
-    }
-
-    template<class F>
-    void enqueue(F&& f) {
-        {
-            std::unique_lock<std::mutex> lock(queue_mutex);
-            tasks.emplace(std::forward<F>(f));
-        }
-        condition.notify_one();
-    }
-
-    void wait() {
-        std::unique_lock<std::mutex> lock(queue_mutex);
-        condition.wait(lock, [this] { return this->tasks.empty() && this->num_idle_threads == workers.size(); });
-    }
-
-    std::vector<bool> get_threads_executing() const {
-        std::vector<bool> result(workers.size());
-        for (size_t i = 0; i < workers.size(); ++i) {
-            result[i] = (i < num_threads_executing);
-        }
-        return result;
-    }
-
-    ~ThreadPool() {
-        {
-            std::unique_lock<std::mutex> lock(queue_mutex);
-            stop = true;
-        }
-        condition.notify_all();
-        for (std::thread& worker : workers)
-            worker.join();
-    }
-
-private:
-    std::vector<std::thread> workers;
-    std::queue<std::function<void()>> tasks;
-    std::mutex queue_mutex;
-    std::condition_variable condition;
-    std::atomic_bool stop;
-    std::atomic_size_t num_idle_threads;
-    std::atomic_size_t num_threads_executing;
-};
-
-
-void task(int i) {
-    std::cout << "Thread " << std::this_thread::get_id() << ": Task " << i << " is running." << std::endl;
-    std::this_thread::sleep_for(std::chrono::seconds(1));
-    std::cout << "Thread " << std::this_thread::get_id() << ": Task " << i << " is done." << std::endl;
-}
+#include <set>
+#include <algorithm>
 
 int main() {
-    try {
-        ThreadPool pool;
-        for (int i = 0; i < 8; ++i) {
-            pool.enqueue([i] { task(i); });
-        }
-        while (true) {
-            std::vector<bool> threads_executing = pool.get_threads_executing();
-            std::cout << "Threads executing: ";
-            for (bool executing : threads_executing) {
-                std::cout << (executing ? "T" : "F") << " ";
-            }
-            std::cout << std::endl;
-            if (pool.get_threads_executing().empty()) {
-                break;
-            }
-            std::this_thread::sleep_for(std::chrono::milliseconds(500));
-        }
-        pool.wait();
+    std::set<int> set1 {1, 2, 3};
+    std::set<int> set2 {3, 2, 1};
+
+    bool isEqual = std::equal(set1.begin(), set1.end(), set2.begin(),
+                              [](const int& a, const int& b) { return a == b; });
+
+    // 输出结果
+    if (isEqual) {
+        std::cout << "set1 and set2 are equal." << std::endl;
+    } else {
+        std::cout << "set1 and set2 are not equal." << std::endl;
     }
-    catch (const std::exception& e) {
-        std::cerr << "Error: " << e.what() << std::endl;
-    }
+
+    std::all_of()
+
     return 0;
 }
