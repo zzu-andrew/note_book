aec0628bfdae5784ce81667df467e7aea774f4dc
zzu-andrew <564631192@qq.com>
2023-12-22 18:32:54 +0800

    添加weak_ptr使用说明

--- src/smart_ptr/weak_ptr.cpp
+++ src/smart_ptr/weak_ptr.cpp
@@ -1,10 +1,9 @@
-#include <cctype>
-#include <string>
+//
+// Created by wangyz38535 on 2023/12/22.
+//
 #include <iostream>
-#include <utility>
 #include <vector>
 #include <map>
-
 #include <memory>
 
 using namespace std;
@@ -24,11 +23,15 @@ public:
     ~Parent() { std::cout << "Parent destructed\n"; }
 };
 
+// 只要上述parent地方为weak_ptr这里无论是否释放parent都能保证两个对象都被释放
 void createCycle() {
     std::shared_ptr<Parent> parent = std::make_shared<Parent>();
     std::shared_ptr<Child> child = std::make_shared<Child>();
     parent->childPtr = child;
     child->parentPtr = parent;
+    std::cout << parent->childPtr.use_count() << std::endl;
+    std::cout << child->parentPtr.use_count() << std::endl;
+
 }
 
 void avoidCycle() {
@@ -36,14 +39,17 @@ void avoidCycle() {
     std::shared_ptr<Child> child = std::make_shared<Child>();
     parent->childPtr = child;
     child->parentPtr = parent;
-    parent.reset(); // 释放parent对象，此时child对象也会被释放，避免了循环引用
+    // 把原有parent释放，这样无论释放哪一个对象都能将对方释放
+    parent.reset(); // 释放parent对象，此时child对象也会被释放 ，避免了循环引用
 }
 
-int main(int argc, char* argv[]) {
 
+int main(int argc, char* argv[]) {
     createCycle(); // 这会导致循环引用，因此Parent和Child的析构函数不会被调用
     avoidCycle(); // 使用std::weak_ptr避免了循环引用，Parent和Child的析构函数会被正确调用
 
+    // 用于去除数组的长度
+    std::cout << std::extent<decltype(argv)>::value  << "  " << std::extent<decltype(argv)>::type::value << std::endl;
 
     return 0;
 }
