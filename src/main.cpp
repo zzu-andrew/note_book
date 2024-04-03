b0a83602b36a1d7d294be07025a7b1f5bd4c2366
zzu-andrew <564631192@qq.com>
2024-04-03 16:22:07 +0800

    增加重载说明

--- src/main.cpp
+++ src/main.cpp
@@ -4,45 +4,56 @@
 #include <utility>
 #include <vector>
 #include <map>
-
+#include <utility>  // for std::forward()
 #include <memory>
 
 using namespace std;
 
-class Child;
-class Parent;
 
-class Child {
+
+class Entry {
 public:
-    std::shared_ptr<Parent> parentPtr;
-    ~Child() { std::cout << "Child destructed\n"; }
+    Entry() {
+        std::cout << "Entry created" << std::endl;
+    }
+    ~Entry() {
+        std::cout << "Entry deleted" << std::endl;
+    }
+
+    static void ShowName() {
+        std::cout << __FUNCTION__ << std::endl;
+    }
+
+    std::string* GetName() {
+        return &name;
+    }
+
+    std::string* operator->() {
+        return &name;
+    }
+
+private:
+    std::string name = "functionname";
+
 };
 
-class Parent {
-public:
-    std::weak_ptr<Child> childPtr;
-    ~Parent() { std::cout << "Parent destructed\n"; }
-};
-
-void createCycle() {
-    std::shared_ptr<Parent> parent = std::make_shared<Parent>();
-    std::shared_ptr<Child> child = std::make_shared<Child>();
-    parent->childPtr = child;
-    child->parentPtr = parent;
-}
-
-void avoidCycle() {
-    std::shared_ptr<Parent> parent = std::make_shared<Parent>();
-    std::shared_ptr<Child> child = std::make_shared<Child>();
-    parent->childPtr = child;
-    child->parentPtr = parent;
-    parent.reset(); // 释放parent对象，此时child对象也会被释放，避免了循环引用
-}
 
 int main(int argc, char* argv[]) {
 
-    createCycle(); // 这会导致循环引用，因此Parent和Child的析构函数不会被调用
-    avoidCycle(); // 使用std::weak_ptr避免了循环引用，Parent和Child的析构函数会被正确调用
+
+
+    {
+        std::shared_ptr<Entry> entry(new Entry());
+        entry->ShowName();
+    }
+
+    Entry  data;
+    // 注意普通
+    data.GetName()->data();
+
+
+
+
 
 
     return 0;
