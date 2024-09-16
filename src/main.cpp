3a4ee485a27bbbaaf7b1ce8518138aa9076dcb5c
zzu-andrew <564631192@qq.com>
2024-09-16 20:16:36 +0800

    添加进程和线程说明

--- src/main.cpp
+++ src/main.cpp
@@ -15,7 +15,7 @@
 #include <iostream>
 #include <unistd.h>
 #include <cerrno>
-
+#include <cstring>
 using namespace std;
 
 #include <iostream>
@@ -35,53 +35,52 @@ using namespace std;
 #include <mutex>
 #include <condition_variable>
 
-std::string to_lowercase(const std::string& str) {
-    std::string lower_str = str;
-    for (char &c : lower_str) {
-        c = std::tolower(static_cast<unsigned char>(c));
-    }
-    return lower_str;
-}
-
-bool removeTrailingCrLf(std::string& str) {
-    if (!str.empty()) {
-        // 检查最后两个字符是否为'\r'和'\n'
-        if (str.back() == '\n' && str[str.size() - 2] == '\r') {
-            // 移除最后两个字符
-            str.erase(str.size() - 2);
-            return true;
-        } else if (str.back() == '\r') {
-            // 如果只有'\r'，则移除最后一个字符
-            str.pop_back();
-            return true;
-        } else if (str.back() == '\n') {
-            // 如果只有'\n'，则移除最后一个字符
-            str.pop_back();
-            return true;
-        }
-    }
-    return false;
-}
 
 
-std::string Rtrim(const std::string& original, const std::string& chars = "\t\n\r\v\f ")
-{
-    std::string str = original;
-    str.erase(str.find_last_not_of(chars) + 1);
-    return str;
-}
+
+class Base {
+public:
+    virtual void func() { std::cout << "Base func()" << std::endl; }
+
+private:
+    int iCount;  // 整数计数
+};
+
+class Derived1 : virtual public Base {
+public:
+    virtual void func() override { std::cout << "Derived1 func()" << std::endl; }
+};
+
+class Derived2 : virtual public Base {
+public:
+    virtual void func() override { std::cout << "Derived2 func()" << std::endl; }
+};
+
+class Combined : public Derived1, public Derived2 {
+public:
+    void func() override { std::cout << "Combined func()" << std::endl; }
+};
+
+
+
+
+
 
 int main() {
 
-    static int primes[] = {509, 509, 1021, 2053, 4093,
-                           8191, 16381, 32771, 65521, INT_MAX
-    };
 
-    // 使用for循环取一个计数值
-    int i;
-    for (i = 1; primes[i] < 65528; i++);
 
-    std::cout << i << "    " << primes[i - 1] << std::endl;
+    Derived1 derived_1;
+    Derived2 derived_2;
+    Combined combined;
+
+
+
+
+
+
+
+
 
 
     return 0;
